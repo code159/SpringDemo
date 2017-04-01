c109c3b2532482c963ce344bef96abfd9d6caf72
code159 <409901403@qq.com>
2017-04-01 22:52:08 +0800

    新增属性注入、构造器注入(type、index)、<![CDATA[]]>

--- src/org/lyb/helloworld/Main.java
+++ src/org/lyb/helloworld/Main.java
@@ -13,9 +13,23 @@ public class Main {
 		
 //		BeanFactory factory=new ClassPathXmlApplicationContext("applicationContext.xml");
 //		HelloWorld hello=(HelloWorld)factory.getBean("HelloWorld");
+		//创建IOC容器
 		ApplicationContext ctx=new ClassPathXmlApplicationContext("applicationContext.xml");
-		HelloWorld hello=(HelloWorld)ctx.getBean("HelloWorld");
-				
+		
+		//利用id定位到IOC容器的bean
+		HelloWorld hello=(HelloWorld)ctx.getBean("HelloWorld"); //getBean方法是在其父接口BeanFactory中定义的，有多种重载形式
 		hello.getHello();
+		//利用类型返回IOC容器中的bean，但要求IOC容器中必须只能有一个该类型的bean
+		HelloWorld hello2=ctx.getBean(HelloWorld.class);
+		System.out.println(hello2);
+		hello2.getHello();
+		
+		Car car=(Car)ctx.getBean("car");
+		System.out.println(car);
+		Car car2=(Car)ctx.getBean("car2");
+		System.out.println(car2);
+		
+		Person person=ctx.getBean(org.lyb.helloworld.Person.class);
+		System.out.println(person);
 	}
 }

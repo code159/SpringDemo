c34cf3aa81d6e00a61a397284a8529fc890a8654
code159 <409901403@qq.com>
2017-04-28 22:24:04 +0800

    新增aop（aspectJ注入）

--- src/sleep/aop/Main.java
+++ src/sleep/aop/Main.java
@@ -6,11 +6,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx=new ClassPathXmlApplicationContext("aopSleep.xml");
+		ApplicationContext ctx=new ClassPathXmlApplicationContext("sleep/aop/aopSleep.xml");
 		Sleepable sleeper=(Sleepable)ctx.getBean("humanProxy");
 		sleeper.sleep();
 		
-		ctx=new ClassPathXmlApplicationContext("autoProxyAopSleep.xml");
+		ctx=new ClassPathXmlApplicationContext("sleep/aop/autoProxyAopSleep.xml");
+		sleeper=(Sleepable)ctx.getBean("human");
+		sleeper.sleep();
+		
+		ctx=new ClassPathXmlApplicationContext("sleep/aop/annotationAopSleep.xml");
 		sleeper=(Sleepable)ctx.getBean("human");
 		sleeper.sleep();
 	}
